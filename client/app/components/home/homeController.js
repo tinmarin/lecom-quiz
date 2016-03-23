71cfd2fe453183dabece1966b5bbd9e2e07f7044
Sergio Marron <sergiolmarron@gmail.com>
2016-03-22 21:41:53 -0400

    more questions added

--- client/app/components/home/homeController.js
+++ client/app/components/home/homeController.js
@@ -124,24 +124,93 @@
               }
             ]
           },{
-            "question": "What shape do hereditary spherocyte RBCs have?",
+            "question": "Where is the defective rbc undergoing hemolysis?",
             "correct": true,
             "answers": [
               {
-                "text": "Bi-concave"
+                "text": "In the bone marrow as the reticulocyte is being formed"
               },
               {
-                "text": "Triangular"
+                "text": "In the capillaries, because the shape is not flexible and the rbc becomes lodged",
+                correct: true
               },
               {
-                "text": "Spherical",
-                "correct" : true
+                "text": "In the alveoli of the lungs because it doesn’t carry enough hemoglobin for gas exchange"
               },
               {
-                "text": "Pyramidal"
+                "text": "in the liver sinusoids by Kupffer cells"
+              }
+          ]
+        },{
+            "question": "How does the aplastic crisis arise?",
+            "correct": true,
+            "answers": [
+              {
+                "text": "As the immature red blood cell is released into the circulation"
               },
               {
-                "text": "Quadrangular"
+                "text": "When the spleen has destroyed enough red blood cells to create anemia"
+              },
+              {
+                "text": "After the hematopoietic cells of the bone marrow reach their full capacity to regenerate reticulocytes and still cannot maintain homeostasis",
+                correct: true
+              },
+              {
+                "text": "Because the liver is destroying red blood cells faster than they can be produced"
+              }
+          ]
+        },{
+            "question": "Where is the megaloblastic crisis occurring?",
+            "correct": true,
+            "answers": [
+              {
+                "text": "In the liver sinusoids, where rbc’s are being destroyed and causing hepatomegaly"
+              },
+              {
+                "text": "In the hematopoietic cells of the bone marrow"
+              },
+              {
+                "text": "In the yellow bone marrow"
+              },
+              {
+                "text": "In the sinusoids of the spleen, causing splenomegaly due to chronic inflammation secondary to the increased rate of rbc destruction",
+                correct: true
+              }
+          ]
+        },{
+            "question": "What forms the gallstones seen in Moderate to Severe Spherocytosis?",
+            "correct": true,
+            "answers": [
+              {
+                "text": "High levels of bilirubin in the peripheral blood due to red blood cell hemolysis",
+                correct: true
+              },
+              {
+                "text": "High blood levels of calcium secondary to red blood cell hemolysis"
+              },
+              {
+                "text": "High blood levels of free hemoglobin released from lysed red blood cells"
+              },
+              {
+                "text": "The spleen releasing large amounts of phospholipids derived from destroyed red blood cell membranes"
+              }
+          ]
+        },{
+            "question": "How is bone marrow function assessed?",
+            "correct": true,
+            "answers": [
+              {
+                "text": "By ordering a complete blood cell count (CBC)"
+              },
+              {
+                "text": "With a lymphocyte and platelet count"
+              },
+              {
+                "text": "With a reticulocyte count to look for immature red blood cells",
+                correct: true
+              },
+              {
+                "text": "By drawing a bone marrow sample from the pelvis"
               }
           ]
         }
